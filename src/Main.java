e62a6b5b7892904da0af746353b71919414d7437
only-changer <only-changer@sjtu.edu.cn>
2018-04-12 00:12:07 +0800

    first bulid

--- src/Main.java
+++ src/Main.java
@@ -437,7 +437,7 @@ public class Main
 
     public static void main(String[] args) throws Exception
     {
-        File f = new File("program.txt");
+        File f = new File("E:/test.txt");
         InputStream input = null;
         input = new FileInputStream(f);
         run(input);
