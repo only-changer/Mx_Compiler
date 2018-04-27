9ad28b445cc6e5658571f7075bc40b202b40df5b
only-changer <only-changer@sjtu.edu.cn>
2018-04-27 18:48:10 +0800

    try_ir

--- semantic.bash
+++ semantic.bash
@@ -5,6 +5,6 @@
 
 set -e
 cd "$(dirname "$0")"
-export CCHK="java -classpath ./lib/antlr-4.7-complete.jar:./bin irtox86"
+export CCHK="java -classpath ./lib/antlr-4.7-complete.jar:./bin Irtox86"
 cat > program.txt   # save everything in stdin to program.txt
 $CCHK
