7447e846ccfbc6fd5ecbbdb7b151ae1b0ba1631a
only-changer <only-changer@sjtu.edu.cn>
2018-05-03 21:45:58 +0800

    irinit

--- src/Irtox86.java
+++ src/Irtox86.java
@@ -112,7 +112,7 @@ public class Irtox86
     {
         Main m = new Main();
         check chk = m.main();
-        chk.code.print();
+       // chk.code.print();
         translate(chk.code);
     }
 }
