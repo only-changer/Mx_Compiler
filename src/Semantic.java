b733c761b07a578b98ba2f3af6297b077ff2f2f7
only-changer <only-changer@sjtu.edu.cn>
2018-05-03 16:14:36 +0800

    irinit

---
+++ src/Semantic.java
@@ -0,0 +1,9 @@
+public class Semantic
+{
+    public static void main(String[] args) throws Exception
+    {
+        Main m = new Main();
+        check chk = m.main();
+        chk.code.print();
+    }
+}
