22ea986392649eb56c725e53ded5e5a41d21098c
only-changer <only-changer@sjtu.edu.cn>
2018-05-14 13:37:31 +0800

    try_10

--- hello.asm
+++ hello.asm
@@ -3,29 +3,34 @@ section   .text
 main:
       mov	rax,5
       mov	rcx,0
-      cmp	rcx,0
-      je	_0else
-_0if:
-      mov	rdx,10
-      jmp	_0ifback
-_0else:
-      mov	rdx,20
-_0ifback:
-      xor	rdx,10
-      not	rdx
-      xor	rax,0
-      not	rax
-      xor	rax,5
-      not	rax
-      mov	rsp,rbp
-      cmp	rsp,0
-      je	_1else
-_1if:
-      mov	rdx,30
-      jmp	_1ifback
-_1else:
-_1ifback:
-      mov	rdi,rdx
+      mov	rdx,1
+_1for:
+      mov	rbx,1
+_0for:
+      add	rcx,rdx
+      add	rbx,1
+      cmp	rbx,rax
+      jle	_0cmp
+      mov	rsp,0
+      jmp	_0cmpback
+_0cmp:
+      mov	rsp,1
+_0cmpback:
+      cmp	rsp,1
+      je	_0for
+_0forback:
+      add	rdx,1
+      cmp	rdx,rax
+      jle	_1cmp
+      mov	rbp,0
+      jmp	_1cmpback
+_1cmp:
+      mov	rbp,1
+_1cmpback:
+      cmp	rbp,1
+      je	_1for
+_1forback:
+      mov	rdi,rcx
       mov	rax,60
       syscall
 section   .bss
